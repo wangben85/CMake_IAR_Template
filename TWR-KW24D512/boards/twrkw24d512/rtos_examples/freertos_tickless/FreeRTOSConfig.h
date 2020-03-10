c2c007726ecce3537c935a70460e471ca2b84363
wangben85 <wangben85@sina.com>
2020-03-10 15:58:55 +0800

    minor update for test the interrupt priority setting impact in FreeRTOSConfig.h
    Signed-off-by: wangben85 <wangben85@sina.com>

--- TWR-KW24D512/boards/twrkw24d512/rtos_examples/freertos_tickless/FreeRTOSConfig.h
+++ TWR-KW24D512/boards/twrkw24d512/rtos_examples/freertos_tickless/FreeRTOSConfig.h
@@ -66,7 +66,6 @@
 
     1 tab == 4 spaces!
 */
-
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
@@ -184,6 +183,9 @@ routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
 INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
 PRIORITY THAN THIS! (higher priorities are lower numeric values. */
 #define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 2
+
+//SW button interrupt priority SW_NVIC_PRIO = 2, if set configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY = 3 here, SW intterupt will not have respond
+//#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 3  // test
 
 /* Interrupt priorities used by the kernel port layer itself.  These are generic
 to all Cortex-M ports, and do not rely on any particular library functions. */
